cd4ed20bba3ed387a8ad0401d37a7d92f6b43d7f
Anirudh Bajaj <91120579+bajajanirudh@users.noreply.github.com>
2022-07-03 21:55:38 +0530

    Delete network directory

--- network/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Transaction, Block, Peer, Lock
-
-
-admin.site.register(Transaction)
-admin.site.register(Block)
-admin.site.register(Peer)
-admin.site.register(Lock)
